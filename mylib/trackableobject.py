d35d69148c14ef96037bfd97e64f1134cb1bc385
Sai Subhakar T <36569367+saimj7@users.noreply.github.com>
2020-08-18 21:18:38 +0200

    added main project files

---
+++ mylib/trackableobject.py
@@ -0,0 +1,10 @@
+class TrackableObject:
+	def __init__(self, objectID, centroid):
+		# store the object ID, then initialize a list of centroids
+		# using the current centroid
+		self.objectID = objectID
+		self.centroids = [centroid]
+
+		# initialize a boolean used to indicate if the object has
+		# already been counted or not
+		self.counted = False
